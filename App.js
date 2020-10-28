094e37fd3f74ecf7c2004adc84ba8baaf9f5dc4f
shafinakandhclear <shafinakandh3@gmail.com>
2020-10-27 20:58:03 -0400

    Login Page

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter username:</Text>
+      <TextInput style={styles.input} />
+      <Text>Enter password:</Text>
+      <TextInput style={styles.input} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +23,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
